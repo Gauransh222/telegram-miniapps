e4d32aea565090d58f48456133625187c9ff6339
gaurav <aditya020967@outlook.com>
2025-12-27 17:06:56 +0530

    dsfhdf

--- main.py
+++ main.py
@@ -33,13 +33,13 @@ DELETE_AFTER = 45*60
 # CONTENT CONFIG
 # -------------------------------
 CONTENT_CONFIG = {
-    "indian1":      {"channel": -1001111111111, "messages": list(range(1,61)), "html": "cpindian1.html"},
-    "indian2":      {"channel": -1002222222222, "messages": list(range(1,61)), "html": "cpindian2.html"},
-    "premium1":     {"channel": -1003333333333, "messages": list(range(1,61)), "html": "cppremium1.html"},
-    "childvideos1": {"channel": -1004444444444, "messages": list(range(1,61)), "html": "cpchildvideos1.html"},
+    "indian1":      {"channel": -1001111111111, "messages": list(range(1,61)), "html": "indian1.html"},
+    "indian2":      {"channel": -1002222222222, "messages": list(range(1,61)), "html": "indian2.html"},
+    "premium1":     {"channel": -1003333333333, "messages": list(range(1,61)), "html": "premium1.html"},
+    "childvideos1": {"channel": -1004444444444, "messages": list(range(1,61)), "html": "childvideos1.html"},
     # hidden
-    "indian1.1":  {"channel": -1005555555555, "messages": list(range(1,61)), "html": "cpindian1_1.html"},
-    "indian1.2":  {"channel": -1006666666666, "messages": list(range(1,61)), "html": "cpindian1_2.html"},
+    "indian1.1":  {"channel": -1005555555555, "messages": list(range(1,61)), "html": "indian1.1.html"},
+    "indian1.2":  {"channel": -1006666666666, "messages": list(range(1,61)), "html": "indian1.2.html"}
 }
 
 VALID_KEYS = set(CONTENT_CONFIG.keys())
